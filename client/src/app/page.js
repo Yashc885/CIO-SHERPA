f758413e5adbed311c7ca8d922fc65eff1360c80
Yashc885 <yashcoudhary88505342@gmail.com>
2024-06-04 13:06:23 +0530

    Initial commit

---
+++ client/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-teal-500 text-center text-2xl font-bold "> hello world </div>
+    </>
+  );
+}
