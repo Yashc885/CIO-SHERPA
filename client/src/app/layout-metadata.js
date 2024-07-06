a4c3c1d1c279c1b66e079f37e40fb10ab03b1370
Yashc885 <yashcoudhary88505342@gmail.com>
2024-07-06 21:57:16 +0530

    built

---
+++ client/src/app/layout-metadata.js
@@ -0,0 +1,7 @@
+// src/app/layout-metadata.js
+
+export const metadata = {
+    title: "CIO Sherpa Council",
+    description: "Community That works together",
+  };
+  
--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,30 +1,30 @@
+'use client';
 import "./globals.css";
-import Head from 'next/head';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-export const metadata = {
-  title: "CIO Sherpa Council",
-  description: "Community That works together",
-};
+import { useEffect } from "react";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const AOS = require("aos");
+      AOS.init();
+    }
+  }, []);
+
   return (
     <html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
-       <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
       </Head>
       <body className="font-merriweather">
         {children}
-        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-          <script>
-            AOS.init();
-          </script>
+        <Script src="https://unpkg.com/aos@next/dist/aos.js" strategy="lazyOnload" />
       </body>
     </html>
   );
